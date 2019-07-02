0ae159df894208ef268955206f681d15afb931cd
Panfilov Aleksei <aleert@yandex.ru>
2019-07-02 15:52:36 +0300

    Add ListSerializerWithoutNull subclass of ListSerializer.

---
+++ courses_platform/common/serializers.py
@@ -0,0 +1,16 @@
+from django.db import models
+from rest_framework import serializers
+
+
+class ListSerializerWithoutNulls(serializers.ListSerializer):
+    """List serializer that will omit null from response if result of item serialization was None."""
+
+    def to_representation(self, data):
+        iterable = data.all() if isinstance(data, models.Manager) else data
+        ret = []
+        for item in iterable:
+            # check only None to keep 0, False and other falsy values
+            if self.child.to_representation(item) is not None:
+                ret.append(self.child.to_representation(item))
+
+        return ret
